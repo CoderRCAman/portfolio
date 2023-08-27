f364b79c48ff5a660d90663d4680cbfa7585043e
CoderRCAman <www.amank.o.12345@gmail.com>
2023-08-28 05:04:03 +0530

    ninth

--- src/components/Projects/Projects.svelte
+++ src/components/Projects/Projects.svelte
@@ -5,8 +5,8 @@
 	import SynchYt from './SynchYT/SynchYT.svelte';
 	import TwitterClone from './Twitter Clone/TwitterClone.svelte';
 	import gsap from 'gsap';
-	import  {ScrollTrigger}  from 'gsap/ScrollTrigger';
-	// import ScrollTrigger from 'gsap/dist/ScrollTrigger';
+	// import  {ScrollTrigger}  from 'gsap/ScrollTrigger';
+	import {ScrollTrigger} from 'gsap/dist/ScrollTrigger';
 	gsap.registerPlugin(ScrollTrigger);
 	onMount(() => {
 		let ivrTl = gsap.timeline({
